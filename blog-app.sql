ceb5fc44380fce74e8b515ff061f82098d0c862c
Sam Gould <sjg326@gmail.com>
2018-01-02 14:36:55 -0800

    updates to blog-app so it has basic functionality

--- blog-app.sql
+++ blog-app.sql
@@ -1,35 +1,33 @@
-
-
 CREATE TABLE users (
-  id integer NOT NULL,
+  id serial PRIMARY KEY,
   first_name text,
   last_name text,
   email_address text NOT NULL,
-  screen_name text PRIMARY KEY NOT NULL
+  screen_name text NOT NULL
 );
 
 CREATE TABLE posts (
   id serial PRIMARY KEY,
   author text REFERENCES users,
-  title text,
+  title text PRIMARY KEY,
   content text,
   published_timestamp DATE DEFAULT CURRENT_DATE,
-  comments INT NOT NULL DEFAULT 0,
-  tags INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE comments (
-  author text REFERENCES users,
-  post_id integer REFERENCES posts,
-  comment_text text NOT NULL,
+  id serial PRIMARY KEY,
+  author_id INT REFERENCES users,
+  post_id INT REFERENCES posts,
+  comment text NOT NULL,
 );
 
-CREATE tags (
-  id serial PRIMARY KEY,
-  tag_text text,
+CREATE TABLE tags (
+  tag_text text NOT NULL,
+  id serial PRIMARY KEY
 );
 
 CREATE TABLE posts_tags (
-  posts_id INT REFERENCES posts,
-  tags_id INT REFERENCES tags
+  post_id INT REFERENCES posts,
+  tag_id INT REFERENCES tags,
+  PRIMARY KEY (post_id, tag_id)
 );
